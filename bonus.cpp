4f3c0b0abc1ebc8de679a0d97c77628519403b50
Matt Agar <mattagar6@gmail.com>
2020-11-06 21:00:45 -0500

    readin input for bonus

--- bonus.cpp
+++ bonus.cpp
@@ -9,11 +9,25 @@ using namespace std;
 #define ford(i, n) for(int i = int(n)-1; i >= 0; i--)
 #define fore(x, v) for(const auto& x : v)
 
+const int nax = 505;
 
+int grid[nax][nax], X[nax*nax], Y[nax*nax], L[nax*nax], R[nax*nax], n, K;
+int dr[] = {-1, 0, 0, 1};
+int dc[] = {0, -1, 1, 0};
+
+
+bool ok(int i, int j) {
+    return 0 <= i && i < n && 0 <= j && j < n;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
+    cin >> n >> K;
+    forn(i, K) {
+        cin >> X[i] >> Y[i] >> L[i] >> R[i];
+        --X[i], --Y[i];
+    }
     
 }
